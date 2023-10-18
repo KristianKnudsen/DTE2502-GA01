644c3c70bb80c890ac375d44fff7b195386a5637
KristianKnudsen <kskdud123@gmail.com>
2023-10-18 15:16:16 +0200

    f

---
+++ modules/loss.py
@@ -0,0 +1,45 @@
+import torch
+
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+
+class PHOSCLoss(nn.Module):
+    def __init__(self, phos_w=4.5, phoc_w=1):
+        super().__init__()
+
+        self.phos_w = phos_w
+        self.phoc_w = phoc_w
+
+    # y represents a dictionary containing the model predictions
+    # It has keys for values phos and phoc.
+    # The targets has the actual values the model is supposed to reach.
+    # these are in Tensor format and has values for the phosc features.
+    # therefore we have to split this up in accordance to the length of the phos and the phoc from the dict.
+    def forward(self, y: dict, targets: torch.Tensor):
+        # Model predictions
+        phos_output = y['phos']
+        phoc_output = y['phoc']
+
+        # 
+        phos_length = y['phos'].shape[1]
+        phoc_length = y['phoc'].shape[1]
+
+        # Actual values
+        phos_target, phoc_target = targets.split([phos_length, phoc_length], dim=1)
+
+        # tf.keras.losses.MSE
+    	# Apply the loss on PHOS features this is a regression loss
+    	# Note: This loss should be applicable to the PHOS part of the 
+    	# output which is the first part of the output.
+        phos_loss = self.phos_w * F.mse_loss(phos_output, phos_target)
+
+        # Cross entropy loss
+        # Apply the loss on PHOC features this is a classification loss
+    	# Note: This loss should be applicable to the PHOC part of the 
+    	# output which is the later part of the output.
+        phoc_loss = self.phoc_w * F.binary_cross_entropy(phoc_output, phoc_target)
+
+        loss = phos_loss + phoc_loss
+        return loss
---
+++ modules/models.py
@@ -0,0 +1,91 @@
+import torch
+
+import torch.nn as nn
+
+from modules.pyramidpooling import TemporalPyramidPooling
+
+from timm.models.registry import register_model
+
+__all__ = [
+    'PHOSCnet_temporalpooling'
+]
+
+
+class PHOSCnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+        self.temporal_pool = TemporalPyramidPooling([1, 2, 5])
+
+        self.phos = nn.Sequential(
+            nn.Linear(4096, 4096),  # The input dimension here needs to match the output size of your pooling layer
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 165),
+            nn.ReLU()
+        )
+
+        self.phoc = nn.Sequential(
+            nn.Linear(4096, 4096),  # The input dimension here needs to match the output size of your pooling layer
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 604),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x: torch.Tensor) -> dict:
+        x = self.conv(x)
+        x = self.temporal_pool(x)
+
+        return {'phos': self.phos(x), 'phoc': self.phoc(x)}
+
+
+@register_model
+def PHOSCnet_temporalpooling(**kwargs):
+    return PHOSCnet()
+
+
+if __name__ == '__main__':
+    model = PHOSCnet()
+
+    x = torch.randn(5, 50, 250, 3).view(-1, 3, 50, 250)
+
+    y = model(x)
+
+    print(y['phos'].shape)
+    print(y['phoc'].shape)
